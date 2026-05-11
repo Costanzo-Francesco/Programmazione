17ef258e9d40bd1347f2d219a822518c2a67aa85
Costanzo-Francesco <fc549788@gmail.com>
2026-05-11 13:24:02 +0200

    codice.c di bitparità

--- progetti/bitParità/code.c
+++ progetti/bitParità/code.c
@@ -4,56 +4,36 @@
 #include <string.h>
 #include <stdlib.h>
 
-void pari(int nCaratteri, char codice[]){
+bool controllo(int nCaratteri, char codice[]) {
 
     int temp, somma = 0, cifraControllo;
     cifraControllo = (int)codice[nCaratteri - 1] - 48;
-    for (int i = 0; i < nCaratteri; i++){
-        if (i != nCaratteri){
+
+    for (int i = 0; i < nCaratteri; i++) {
+        if (i != nCaratteri) {
             temp = (int)(codice[i] - 48);
             somma += temp;
         } 
     }
 
-    if ((somma % 2) == cifraControllo) {
-        printf("\nCORRETTO\n");
-    } else {
-        printf("\nERRATO\n");
-    }
-
-    printf("---\t---\nsomma: %d\nbit: %d\n", somma, cifraControllo);
-
+    return (somma % 2) == cifraControllo;
 }
 
-void dispari (int nCaratteri, char codice[]){
-
-    int temp, somma = 0, cifraControllo;
-    cifraControllo = (int)codice[nCaratteri - 1] - 48;
-
-    for (int i = 0; i < nCaratteri; i++){
-        if (i != nCaratteri){
-            temp = (int)(codice[i] - 48);
-            somma += temp;
-        }   
-    }
-    // parità dispari
-    if ((somma % 2) != cifraControllo) {
-        printf("\nCORRETTO\n");
-    } else {
-        printf("\nERRATO\n");
-    }
-
-    printf("---\t---\nsomma: %d\nbit: %d\n", somma, cifraControllo);
-}
-
-int main (int argc, char *argv[]){
+bool main (int argc, char *argv[]) {
     
     int nCaratteri = strlen(argv[1]);
     char codice[nCaratteri + 1];
+    bool flag;
+
     strcpy(codice, argv[1]);
-    if (*argv[2] == '0'){
-        pari(nCaratteri, codice);
-    } else if(*argv[2] == '1'){
-        dispari(nCaratteri, codice);
+
+    if (*argv[2] == '0') {
+        flag = controllo(nCaratteri, codice);
+    } else if(*argv[2] == '1') {
+        flag = !controllo(nCaratteri, codice);
     }
+
+    printf("\nIl flag è: %d", flag);
+
+    return flag;
 }
